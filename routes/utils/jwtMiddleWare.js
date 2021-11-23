bdcfa49d886f252b8747f1564d153f20f5fadcc4
Jvaz0715 <James.Vazquez@codeimmersives.com>
2021-11-23 08:15:25 -0500

    creates jwtMiddleware.js file for authorizations

---
+++ routes/utils/jwtMiddleWare.js
@@ -0,0 +1,29 @@
+const jwt = require("jsonwebtoken");
+
+async function checkJwtToken(req, res, next) {
+   try {
+      if (req.headers && req.headers.authorization) {
+         let jwtToken = req.headers.authorization.slice(7);
+
+         /*
+            the private jwt key is coming from our project. If the decodedJwt token is good, we go to the next parameter in our controller functions.
+         */
+
+         let decodedJwt = jwt.verify(
+            jwtToken,
+            process.env.PRIVATE_JWT_KEY,
+         );
+         next();
+      } else {
+         throw {
+            message: "You do not have permission!",
+            statusCode: 500
+         };
+      }
+   } catch(e) {
+      return next(e);
+      /* refer to errorController */
+   }
+};
+
+module.exports = checkJwtToken;
