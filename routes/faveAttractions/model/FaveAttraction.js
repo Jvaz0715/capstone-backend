83533c606dfff24f7564dfde47960a78cf343949
Jvaz0715 <James.Vazquez@codeimmersives.com>
2021-12-11 14:05:17 -0500

    handles new favorites data

--- routes/faveAttractions/model/FaveAttraction.js
+++ routes/faveAttractions/model/FaveAttraction.js
@@ -3,6 +3,39 @@ const mongoose = require("mongoose");
 const faveAttractionSchema = new mongoose.Schema({
    xid: {
       type: String,
+      unique: true,
+   },
+   attractionName: {
+      type: String,
+      unique: true,
+   },
+   city:{
+      type: String,
+      
+   },
+   states:{
+      type: String,
+      
+   },
+   country:{
+      type: String,
+      
+   },
+   image: {
+      type: String,
+      
+   },
+   attractionInfo: {
+      type: String,
+      
+   },
+   externalURL: {
+      type: String,
+      
+   },
+   wikiPageURL: {
+      type: String,
+      
    },
 });
 
